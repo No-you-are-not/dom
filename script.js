6f98765c70f02e1923caa982238ace8ee6c75bfe
Polina <polina.doctorwho@gmail.com>
2021-11-04 13:04:40 +0200

    fixed

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const topText = document.getElementById('top-text');
-const bottomText = document.getElementById('bottom-text');
-const block = document.getElementById('block');
+const container = document.getElementById('container');
+let title = document.createElement('h2');
+title.setAttribute('class', 'title')
+title.innerText = 'События';
+let outer = document.createElement('div');
+outer.setAttribute('class', 'outer');
+let topText = document.createElement('span');
+topText.innerText = 'x: 0, y: 0';
+let bottomText = document.createElement('span');
+bottomText.innerText ='x: 0, y: 0';
+let block = document.createElement('div');
+block.setAttribute('class', 'block');
+
+outer.appendChild(topText);
+outer.appendChild(block);
+outer.appendChild(bottomText);
+container.appendChild(title);
+container.appendChild(outer);
+
+
+// const topText = document.getElementById('top-text');
+// const bottomText = document.getElementById('bottom-text');
+// const block = document.getElementById('block');
 
 function getXY (event){
     let x = event.clientX;
@@ -11,8 +31,8 @@ function getXYblock (event){
     let x = event.clientX;
     let y = event.clientY;
     console.log('gg')
-    bottomText.innerText = `x: ${x}, y: ${y-44}` // subtracted 44 because if we consider top of the block - the beginning of y coordinates
-    // , it is situated 44 px lower than the actual beginning of y coordinates (because of the title and top coordinates)
+    bottomText.innerText = `x: ${x}, y: ${y-45}` // subtracted 44 because if we consider top of the block - the beginning of y coordinates
+    // , it is situated 45 px lower than the actual beginning of y coordinates (because of the title and top coordinates + 1px border)
 }
 
 document.addEventListener('mousemove', getXY);
